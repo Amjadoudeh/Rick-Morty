a8b2cba0033670bbcc6b0f5e6bd874808c6bff2e
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-12-27 23:02:05 +0100

    Constructed url for the api request in string format

--- RickAndMorty/APIClient/RMRequest.swift
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -2,16 +2,55 @@ import Foundation
 
 /// Object that represents a single API call
 final class RMRequst {
+    
+   
     /// Base URL
     private struct Constants {
         static let baseUrl = "https://rickandmortyapi.com/api"
     }
     
     /// Endponts
+    private let endpoint: RMEndpoint
     
     /// Path components
-
-    /// Query parameters
+    private let pathComponents: [String]
     
-    ///
+    /// Query parameters
+    private let queryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api request in string format
+    public var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach ({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value))"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+        
+    }
+    
+    public init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
