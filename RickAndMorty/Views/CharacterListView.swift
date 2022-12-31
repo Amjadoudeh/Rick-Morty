c9af5e8a343ab99a79ed657de39c07add096b98f
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-12-31 14:48:12 +0100

    Create a collectionView

--- RickAndMorty/Views/CharacterListView.swift
+++ RickAndMorty/Views/CharacterListView.swift
@@ -6,6 +6,7 @@ final class CharacterListView: UIView {
     
     private let viewModel = CharacterListViewViewModel()
     
+    /// Spinner view
     private let spinner: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .large)
         spinner.hidesWhenStopped = true
@@ -13,13 +14,29 @@ final class CharacterListView: UIView {
         return spinner
     }()
     
+    /// Collection View
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isHidden = true
+        collectionView.alpha = 0
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemGray
+        addSubview(collectionView)
         addSubview(spinner)
         addConstraints()
         spinner.startAnimating()
+        viewModel.fetchCharacter()
+        
     }
     
     required init?(coder: NSCoder) {
