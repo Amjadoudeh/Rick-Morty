0129d7807abb20b944da0e0d0ed033d45421b8fe
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2023-01-02 15:31:37 +0100

    Create imageView and labelViews- setup the prepareForReuse function

--- RickAndMorty/Views/RMCharacterCollectionViewCell.swift
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -3,9 +3,36 @@ import UIKit
 class RMCharacterCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterCollectionViewCell"
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    //MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubViews(imageView,nameLabel,statusLabel)
+        addConstraint()
     }
     
     required init?(coder: NSCoder) {
@@ -21,6 +48,10 @@ class RMCharacterCollectionViewCell: UICollectionViewCell {
     /// preaper the cell to be reusable
     override func prepareForReuse() {
         super.prepareForReuse()
+        /// get ride of the image
+        imageView.image = nil
+        nameLabel.text = nil
+        statusLabel.text = nil
     }
     
     /// Configure the cell with the viewModel
