12c075da13a900d5c23fd98c46df301e4ea2a6b6
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2023-01-03 20:23:05 +0100

    Show dynamic data on the screen for the characters

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -2,15 +2,34 @@ import Foundation
 import UIKit
 
 final class RMCharacterListViewViewModel: NSObject  {
+    
+    private var characters: [RMCharacters] = [] {
+        didSet {
+            for character in characters {
+                let viewModel = RMCharacterCollectionViewCellViewModel(
+                    characterName: character.name,
+                    characterStatus: character.status,
+                    characterImageUrl: URL(string: character.image)
+                )
+                cellViewModels.append(viewModel)
+            }
+            
+        }
+    }
+    
+    private var cellViewModels: [RMCharacterCollectionViewCellViewModel] = []
+    
     func fetchCharacter() {
-        RMService.shared.execute(.listCharactersRequest,
-                                 expecting: RMGetAllCharactersResponse.self) { result in
+        RMService.shared.execute(
+            .listCharactersRequest,
+            expecting: RMGetAllCharactersResponse.self
+        ) { [weak self] result in
             switch result {
-            case .success(let model):
-                print(String(model.results.first?.image ?? "no image"))
-//                print(String(model.info.pages))
-//                print(String(model.results.count))
-//                print(String(describing: model))
+                
+            case .success(let resopnseModel):
+                let results = resopnseModel.results
+                //                let info = resopnseModel.info
+                self?.characters = results
                 
             case .failure(let error):
                 print(String(describing: error))
@@ -21,17 +40,15 @@ final class RMCharacterListViewViewModel: NSObject  {
 
 extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return cellViewModels.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterCollectionViewCell else {
             fatalError("unsupported cell")
         }
-        let viewModel = RMCharacterCollectionViewCellViewModel(characterName: "Amjad",
-                                                               characterStatus: .alive,
-                                                               characterImageUrl: URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"))
-        cell.configure(with: viewModel)
+        cell.configure(with: cellViewModels[indexPath.row]
+        )
         
         return cell
     }
@@ -44,5 +61,5 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
             height: width * 1.5
         )
     }
-     
+    
 }
